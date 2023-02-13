4bef897f59094d405def89ea0cc06441c86a395b
ArkFreestyle <arkfreestyle95@gmail.com>
2023-02-13 20:10:27 +0500

    Refactored stip_write_to_file()
    Former-commit-id: 5282c429362e0e8b8e3d196ba69d7a535da467b3

--- os-sim/src/stip-fileutils.c
+++ os-sim/src/stip-fileutils.c
@@ -3,34 +3,24 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#include <stdio.h>
+// #include <stdio.h>
 
-void stip_write_to_file(gchar* data_to_write, gchar* filename, int open_flags)
+void stip_write_to_file(gchar *insert, gchar *filename, int open_flags)
 {
-//   int fd = open("alert-information.txt", O_CREAT | O_RDWR | O_APPEND, S_IRWXU);
   int fd = open(filename, open_flags, S_IRWXU);
-  printf("fd: %d\n", fd);
   if (fd < 0)
-    {
-      printf("stip_write_alarm_to_file: Could not open, error: % d\n", errno);
-      perror("perror: ");
-    }
-  
-  int rv = write(fd, "\n", strlen("\n"));
-  if (rv < 0)
   {
-    printf("stip_write_alarm_to_file: Could not write, error: % d\n", errno);
-    perror("perror: ");
+    g_message("%s: Could not open file, errno:%d, msg:%s", __func__, errno, strerror(errno));
   }
   
-  rv = write(fd, (gchar*)data_to_write, strlen((gchar*)data_to_write));
-  printf("data_to_write: %s\n", (char*)data_to_write);
-  // int rv = write(fd, "hello world", strlen("hello world"));
+  gchar *data_to_write = g_strconcat (insert, "\n", NULL);
+  
+  int rv = write(fd, data_to_write, strlen(data_to_write));
   if (rv < 0)
   {
-    printf("stip_write_alarm_to_file: Could not write, error: % d\n", errno);
-    perror("perror: ");
+    g_message("%s: Could not write to file, errno:%d, msg:%s", __func__, errno, strerror(errno));
   }
   
+  g_free(data_to_write);
   close(fd);
 }
