7848283a4e0f27c0ccc2c3ce07744b60e4c3b599
ArkFreestyle <arkfreestyle95@gmail.com>
2023-03-31 19:53:09 +0500

    Autogenerated config.h instead of what we manually created.
    Former-commit-id: 73e6c8248ebb1d4cb6bb0e27d73a38b3b7c0d5d6

--- os-sim/config.h
+++ os-sim/config.h
@@ -1,2 +1,81 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Debug enabled */
+/* #undef DEBUG_ENABLED */
+
+/* Define this to 1 if you want to enable generation of DCE compliant UUIDs.
+   */
+#define ENABLE_UUID 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if IPv6 is potentially available */
+/* #undef HAVE_IPV6 */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if struct sockaddr has sa_len field */
+/* #undef HAVE_SOCKADDR_SA_LEN */
+
+/* Whether you have struct sockaddr_storage */
+/* #undef HAVE_SOCKADDR_STORAGE */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "os-sim"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "os-sim"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "os-sim "
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "os-sim"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Port where the servere listen for API calls */
 #define SERVER_API_PORT 40009
-#define SERVER_VERSION "420"
+
+/* Version that identifies the server */
+#define SERVER_VERSION "custom_build-d4602b402153439fe9612172a52c3089f7b7a547"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Unit tests enabled */
+/* #undef USE_UNITTESTS */
+
+/* Version number of package */
+#define VERSION ""
